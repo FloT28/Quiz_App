119629dc44fcc7952c350c893a684988300f455b
Florence T <florence.taele@gmail.com>
2026-04-08 23:43:30 +1200

    new changes to files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(
     host=os.getenv("REDIS_HOST", "redis"),
-    port=int(os.getenv("REDIS_PORT", "6739")),
+    port=int(os.getenv("REDIS_PORT", "6379")),
 )
 
 @app.route("/")
